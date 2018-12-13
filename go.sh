c4d06875fffad2cb03e5ace526279c2bed54c315
irfnrdh <kang.fand@gmail.com>
2018-12-13 14:06:47 +0000

    Up

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-git add -u && git add . && git commit -m "-_-"  && git pull origin master && git push origin master
+git add -u && git add . && git commit -m "Up"  && git pull origin master && git push origin master
 
